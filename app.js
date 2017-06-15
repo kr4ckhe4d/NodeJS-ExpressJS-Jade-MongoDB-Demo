71fdb85c1c23388993291d873d8e872e09255832
Nipuna <nipuna@app-monkeyz.com>
2017-06-15 17:58:15 +0530

    Setup basic jade serve

---
+++ app.js
@@ -0,0 +1,38 @@
+// Init express framework.
+var express = require('express');
+var path = require('path');
+
+// Setup express.
+var app = express();
+
+// where the root (server) is located. Needed when including stylesheets and scripts in jade file.
+app.use(express.static(path.join(__dirname, '/')));
+
+// Path to jade views.
+// Not required, because /views is the default folder for jade view-files.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+var data = {
+    firstName: "Hans",
+    lastName: "Jensen",
+    age: 24
+}
+
+// Index route
+app.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'The front page'
+    });
+});
+
+// Init start server function
+startServer();
+
+// Start up the server
+function startServer() {
+    var server = app.listen(8000, function() {
+        var port = server.address().port;
+        console.log('Listening on port ' + port);
+    })
+}
